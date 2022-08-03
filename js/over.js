d4cbda352881820fa01988adba9575cca7584b35
unknown <sadeleye69@gmail.com>
2022-08-03 02:50:13 -0700

    index page

---
+++ js/over.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+    type: "progressbar",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+
+AOS.init();
